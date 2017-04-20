71b17985d012c0a539c5bd7c8c3d32fb6d359b84
Akshay Sadarangani <akshay.sadarangani@gmail.com>
2017-04-20 01:25:10 -0400

    viewport

--- public/js/print.js
+++ public/js/print.js
@@ -26,17 +26,22 @@ function downloadInfographic() {
     });
 }
 
-function generateThumbnail() {
+function generateImage() {
     window.html2canvas([$('#page-content-wrapper')[0]], {
         onrendered: function(canvas) {
             var extra_canvas = document.createElement("canvas");
-            extra_canvas.setAttribute('width',300);
-            extra_canvas.setAttribute('height',424);
+            extra_canvas.setAttribute('width',2480);
+            extra_canvas.setAttribute('height',3508);
             var ctx = extra_canvas.getContext('2d');
-            ctx.drawImage(canvas,0,0,canvas.width, canvas.height,0,0,300,424);
+            ctx.drawImage(canvas,0,0,canvas.width, canvas.height,0,0,2480,3508);
             var dataURL = extra_canvas.toDataURL();
             var img = $(document.createElement('img'));
             img.attr('src', dataURL);
+            var base64image = extra_canvas.toDataURL("image/png");
+            $(img).addClass('img-responsive');
+            // Open the image in a new window
+            $('#page-content-wrapper').css('background-image', 'url('+img+')');
+            //window.open(base64image , "_blank");
             // insert the thumbnail at the top of the page
            // $('body').prepend(img);
         }
